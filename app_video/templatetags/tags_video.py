ae33b9838d20e6f80d2ca2706a40854fe9d2581e
sergey kalinin <core.editor>
2014-11-21 17:54:09 +0400

    related compl

--- app_video/templatetags/tags_video.py
+++ app_video/templatetags/tags_video.py
@@ -2,13 +2,21 @@ from django import template
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
+from kinopom.models import Entry
+
 register = template.Library()
 	
 
 @register.inclusion_tag("part_related_video.html")
-def part_related_video(tags):
+def part_related_video(tags, id_basic_video):
+	tags_list = [tag.id for tag in tags]
+
+	entries_related_video = Entry.objects.filter(tags__pk__in=tags_list)[:7]
+
 	return {
-		'qqq': 222,
+		'entries_related_video': entries_related_video,
+		'tags_list': tags_list,
+		'id_basic_video': id_basic_video,
 		'tags_len': len(tags),
 	}
 
